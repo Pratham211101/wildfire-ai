00fb4230a4555c9f050db81cd0402b38669421ff
Pratham Goswami <goswamipratham21112001@gmail.com>
2025-12-02 22:14:32 +0530

    testing deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,14 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Wildfire Prediction WebApp</h1>
-      <p>Backend says: {message}</p>
-    </div>
+    <>
+      <h1 className="bg-red-400 text-white">WILDFIRE AI</h1>
+      <div className="App">
+        <h1>Wildfire Prediction WebApp</h1>
+        <p>Backend says: {message}</p>
+      </div>
+    </>
+    
   );
 }
 //test deploy
